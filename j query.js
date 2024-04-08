993a2ff188a8b501e059ae8ce79614506646055f
PRATIK98244 <135405472+PRATIK98244@users.noreply.github.com>
2024-04-08 11:10:41 +0530

    Add files via upload

---
+++ j query.js
@@ -0,0 +1,18 @@
+// $(document).redy(function()
+// {
+//     $(".great").css({
+//         'width':'20%','height':'200px','border':'3px solid red'
+//     })
+// })
+
+
+$(document).ready(function(){
+    $(".body").css({'border':'3px solid red','width':'100%','height':'460px','padding':'2px'})
+    $(".great").css({'border': '3px solid red',
+    'color': 'red', 'width':'95%', 'height':'400px','padding':'25px'})
+    $(".grandp").css({'border':'3px solid red','width':'45%', 'height':'340px' ,'margin-left':'10px', 'padding':'25px'})
+    $(".grandp1").css({'border':'3px solid red','width':'90%', 'height':'280px' ,'margin-left':'10px','padding':'25px'})
+    $(".grandp2").css({'border':'3px solid red','width':'97%', 'height':'250px' ,'margin-left':'10px'})
+
+    $(".span").css({'border':'2px solid grey','width':'70%','height':'20px','margin-top':'50px','margin-left':'10px','color':'grey','padding':'10px'})
+})
